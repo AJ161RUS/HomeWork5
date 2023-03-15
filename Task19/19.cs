568d942b585d0b3580e9a6c5aae42ead899a988a
Andrey Zhulay <zhulay.00@mail.ru>
2023-03-15 17:56:16 +0300

    first commit

---
+++ Task19/19.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число 
+и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+int A;
+Console.Write("Введите число: ");
+A = int.Parse(Console.ReadLine()!);
+
+int total = 0 , B, rev;
+B = A;
+
+while(A > 0)
+{
+rev = A % 10;
+total = (total * 10) + rev;
+A = A / 10;
+}
+if(B == total)
+Console.Write("Да");
+else
+Console.Write("Нет");
+
